824924a47fb0c766737b3a6398854fffe227459c
snakepatch53 <snakepatch53@gmail.com>
2024-04-25 19:43:49 -0500

    Reestructuracion de componentes en el dashboard de ventas
    Changes to be committed:
    	deleted:    src/panel.pages/Business.jsx
    	deleted:    src/panel.pages/Carts.jsx
    	deleted:    src/panel.pages/Categories.jsx
    	modified:   src/panel.pages/Home.jsx
    	deleted:    src/panel.pages/Orders.jsx
    	deleted:    src/panel.pages/Shop.jsx
    	deleted:    src/services/businesses.js
    	deleted:    src/services/carts.js
    	deleted:    src/services/categories.js
    	deleted:    src/services/productCarts.js
    	deleted:    src/views/Home.jsx

--- src/panel.pages/Home.jsx
+++ src/panel.pages/Home.jsx
@@ -8,70 +8,88 @@ export default function Home() {
     return (
         <PageContent className="flex flex-col xl:flex-row w-full p-3 gap-3">
             <div className="flex-1">
-                <div className="flex-1 flex flex-col gap-4 bg-[--c3] p-4 rounded-md border-solid border-t-2 border-t-[--c3-txt2] ">
-                    <Search placeholder="Busca por código, nombre o categoría" />
-                    <div className="grid lg:grid-cols-2 gap-2">
-                        <Item />
-                        <Item />
-                        <Item />
-                        <Item />
-                    </div>
-                </div>
+                <SectionProducts />
             </div>
             <div className="flex-1 flex flex-col gap-3 xl:max-w-[600px] ">
-                <div className=" flex flex-col gap-2 bg-[--c3] p-4 rounded-md border-solid border-t-2 border-t-[--c3-txt2] ">
-                    <Search placeholder="Busca por nombr o cédula" />
-                    <span className="block p-3 rounded-md text-[--c3-txt] text-center opacity-70 border-solid border border-black/30 ">
-                        Consumidor final
-                    </span>
-                </div>
-                <div className="flex-1 flex flex-col gap-4 bg-[--c3] p-4 rounded-md border-solid border-t-2 border-t-[--c3-txt2] ">
-                    <table className="text-center border-collapse" border="1">
-                        <thead className="text-[--c3-txt2] uppercase ">
-                            <tr>
-                                <th className="font-sans">Producto</th>
-                                <th className="font-sans">Cantidad</th>
-                                <th className="font-sans">Precio</th>
-                                <th className="font-sans">Subtotal</th>
-                                <th className="font-sans">Acción</th>
-                            </tr>
-                        </thead>
-                        <tbody className=" text-[--c3-txt] ">
-                            <TableProductItem />
-                            <TableProductItem />
-                            <TableProductItem />
-                            <TableProductItem />
-                        </tbody>
-                        <tfoot className=" font-bold text-[--c3-txt2] uppercase ">
-                            <tr>
-                                <td colSpan="2"></td>
-                                <td className="font-sans">SUBTOTAL:</td>
-                                <td className="font-sans">$80.00</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td colSpan="2"></td>
-                                <td className="font-sans">TAX (12%):</td>
-                                <td className="font-sans">$10.00</td>
-                                <td></td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                    <Button
-                        as="button"
-                        text="Cobrar $90"
-                        className="gap-0 bg-[--c1] text-[--c1-txt] "
-                        classText="font-sans text-md"
-                    />
-                </div>
+                <SectionClient />
+                <SectionSale />
             </div>
         </PageContent>
     );
 }
 
+function SectionProducts() {
+    return (
+        <div className="flex-1 flex flex-col gap-4 bg-[--c3] p-4 rounded-md border-solid border-t-2 border-t-[--c3-txt2] ">
+            <Search placeholder="Busca por código, nombre o categoría" />
+            <div className="grid lg:grid-cols-2 gap-2">
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+            </div>
+        </div>
+    );
+}
+
+function SectionClient() {
+    return (
+        <div className=" flex flex-col gap-2 bg-[--c3] p-4 rounded-md border-solid border-t-2 border-t-[--c3-txt2] ">
+            <Search placeholder="Busca por nombr o cédula" />
+            <span className="block p-3 rounded-md text-[--c3-txt] text-center opacity-70 border-solid border border-black/30 ">
+                Consumidor final
+            </span>
+        </div>
+    );
+}
+
+function SectionSale() {
+    return (
+        <div className="flex-1 flex flex-col gap-4 bg-[--c3] p-4 rounded-md border-solid border-t-2 border-t-[--c3-txt2] ">
+            <table className="text-center border-collapse" border="1">
+                <thead className="text-[--c3-txt2] uppercase ">
+                    <tr>
+                        <th className="font-sans">Producto</th>
+                        <th className="font-sans">Cantidad</th>
+                        <th className="font-sans">Precio</th>
+                        <th className="font-sans">Subtotal</th>
+                        <th className="font-sans">Acción</th>
+                    </tr>
+                </thead>
+                <tbody className=" text-[--c3-txt] ">
+                    <TableProductItem />
+                    <TableProductItem />
+                    <TableProductItem />
+                    <TableProductItem />
+                </tbody>
+                <tfoot className=" font-bold text-[--c3-txt2] uppercase ">
+                    <tr>
+                        <td colSpan="2"></td>
+                        <td className="font-sans">SUBTOTAL:</td>
+                        <td className="font-sans">$80.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2"></td>
+                        <td className="font-sans">TAX (12%):</td>
+                        <td className="font-sans">$10.00</td>
+                        <td></td>
+                    </tr>
+                </tfoot>
+            </table>
+            <Button
+                as="button"
+                text="Cobrar $90"
+                className="gap-0 bg-[--c1] text-[--c1-txt] "
+                classText="font-sans text-md"
+            />
+        </div>
+    );
+}
+
 function Search({ placeholder }) {
     return (
-        <div className="rounded-md ">
+        <div className="rounded-md">
             <div className="w-full flex items-center gap-1 bg-[--c3] p-3 rounded-md text-white">
                 <input
                     className="flex-1 bg-transparent"
@@ -81,6 +99,35 @@ function Search({ placeholder }) {
                 <FontAwesomeIcon icon={faSearch} />
             </div>
         </div>
+    );
+}
+
+function Item({ name, category, stock, price }) {
+    return (
+        <button className="flex gap-2 p-3 bg-[--c3] rounded-md cursor-pointer transition-all hover:scale-[1.03] hover:border-2 hover:border-[--c3-txt] hover:shadow-lg">
+            <div className="h-full aspect-square rounded-md ">
+                <img
+                    src="https://via.placeholder.com/70"
+                    alt="Producto"
+                    className="w-full h-full object-cover rounded-md"
+                />
+            </div>
+            <div className="flex-1 flex flex-col items-start overflow-hidden ">
+                <h3 className=" block w-full max-w-full font-sans leading-5 text-left text-lg text-[--c3-txt] text-ellipsis text-nowrap overflow-hidden">
+                    {name || "Nombre del producto"}
+                </h3>
+                <p className="font-sans text-[--c3-txt2] text-ellipsis leading-5 text-nowrap overflow-hidden opacity-85 ">
+                    {category || "Categoría"}
+                </p>
+                <span className="text-[--c3-txt2] opacity-75 leading-5">
+                    <b className="font-sans text-sm ">Stock: </b>
+                    {stock || 0}
+                </span>
+                <span className="font-josefin font-bold text-xl text-[--c3-txt3] opacity-75 leading-5">
+                    {price || "$0.00"}
+                </span>
+            </div>
+        </button>
     );
 }
 
@@ -120,34 +167,5 @@ function TableProductItem() {
                 </div>
             </td>
         </tr>
-    );
-}
-
-function Item({ name, category, stock, price }) {
-    return (
-        <button className="flex gap-2 p-3 bg-[--c3] rounded-md cursor-pointer transition-all hover:scale-[1.03] hover:border-2 hover:border-[--c3-txt] hover:shadow-lg">
-            <div className="h-full aspect-square rounded-md ">
-                <img
-                    src="https://via.placeholder.com/70"
-                    alt="Producto"
-                    className="w-full h-full object-cover rounded-md"
-                />
-            </div>
-            <div className="flex-1 flex flex-col items-start overflow-hidden ">
-                <h3 className=" block w-full max-w-full font-sans leading-5 text-left text-lg text-[--c3-txt] text-ellipsis text-nowrap overflow-hidden">
-                    {name || "Nombre del producto"}
-                </h3>
-                <p className="font-sans text-[--c3-txt2] text-ellipsis leading-5 text-nowrap overflow-hidden opacity-85 ">
-                    {category || "Categoría"}
-                </p>
-                <span className="text-[--c3-txt2] opacity-75 leading-5">
-                    <b className="font-sans text-sm ">Stock: </b>
-                    {stock || 0}
-                </span>
-                <span className="font-josefin font-bold text-xl text-[--c3-txt3] opacity-75 leading-5">
-                    {price || "$0.00"}
-                </span>
-            </div>
-        </button>
     );
 }
